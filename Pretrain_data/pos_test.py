45df2caec384a9bc5730fb8e50035f50ce638aa1
LeoWood <463861936@qq.com>
2020-05-12 21:35:38 +0800

    pos_tools

--- Pretrain_data/pos_test.py
+++ Pretrain_data/pos_test.py
@@ -1,49 +1,59 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 # Author: LiuHuan
 # Datetime: 2020/5/12 17:12
 
 import time
 
 import pkuseg
-pku_seg = pkuseg.pkuseg(model_name='medicine',postag=True)
+
+pku_seg = pkuseg.pkuseg(model_name='medicine', postag=True)
+
+
 def pku(line):
     pku_result = pku_seg.cut(line)
-    return [(word,tag) for (word,tag) in pku_result]
+    return [(word, tag) for (word, tag) in pku_result]
 
 
 import thulac
+
 thup = thulac.thulac()
+
+
 def thu(line):
     thulac_result = thup.cut(line)
-    return [(word,tag) for (word,tag) in thulac_result]
+    return [(word, tag) for (word, tag) in thulac_result]
 
 
 from pyhanlp import HanLP
+
+
 def han(line):
     hanlp_resut = HanLP(line)
-    return [(term.word,term.nature) for term in hanlp_resut]
+    return [(term.word, term.nature) for term in hanlp_resut]
 
 
 from stanfordcorenlp import StanfordCoreNLP
-stanford_nlp = StanfordCoreNlp(r'F:\LiuHuan\stanford-corenlp-4.0.0',lang='zh')
+
+stanford_nlp = StanfordCoreNlp(r'F:\LiuHuan\stanford-corenlp-4.0.0', lang='zh')
+
+
 def stanford(line):
     stanford_resut = stanford_nlp.pos_tag(line)
-    return [(word,tag) for (word,tag) in stanford_resut]
+    return [(word, tag) for (word, tag) in stanford_resut]
 
 
-with open('R_test.txt','r',encoding='utf-8') as f:
+with open('R_test.txt', 'r', encoding='utf-8') as f:
     lines = [line.strip() for line in f.readlines() if line.strip()]
 
-for pos in [pku,thu,han,stanford]:
+for pos in [pku, thu, han, stanford]:
     t1 = time.time()
-    with open(str(pos) +'.txt','w',encoding='utf-8') as f:
+    with open(str(pos) + '.txt', 'w', encoding='utf-8') as f:
         for line in lines:
             f.write(str(pos(line)) + '\n')
         t2 = time.time()
-        print(str(pos) + "用时：" + str(t2-t1))
-        f.write(str(pos) + "用时：" + str(t2-t1))
-
+        print(str(pos) + "用时：" + str(t2 - t1))
+        f.write(str(pos) + "用时：" + str(t2 - t1))
 
 if __name__ == '__main__':
     pass
